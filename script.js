c17027398fe8267f3067a965dc40c656aec6b71d
Shell <shellfarley@gmail.com>
2021-10-01 11:04:05 +1000

    updated interaction on projects on iPad and iPhone screen sizes, index, script and css updated for this

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ const navUl = document.getElementById('nav');
 hamburger.addEventListener('click', () => {
   navUl.classList.toggle('show');
 })
+
+// Mobile project hover effect
+const projectOverlay = document.getElementsByClassName("project__overlay")[0]
+
+projectOverlay.addEventListener("touchstart", function() {
+  projectOverlay.classList.add("project__overlay--mobile-hover")
+})
+
+projectOverlay.addEventListener("touchend", function() {
+  projectOverlay.classList.remove("project__overlay--mobile-hover")
+})
