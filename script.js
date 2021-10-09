440d482767e8beeb0ca3c6dfe4c07d497ec2e787
Shell <shellfarley@gmail.com>
2021-10-09 20:13:58 +1100

    mobile hover js fix

--- script.js
+++ script.js
@@ -7,45 +7,19 @@ hamburger.addEventListener('click', () => {
 })
 
 // Mobile project hover effect
-const projectOverlay = document.getElementsByClassName("project__overlay")[0]
 
-projectOverlay.addEventListener("touchstart", function() {
-  projectOverlay.classList.add("project__overlay--mobile-hover")
-})
+document.querySelectorAll('.project__overlay').forEach(function(imageHoverOn) {
+  imageHoverOn.addEventListener('touchstart', function() {
+   imageHoverOn.classList.add("project__overlay--mobile-hover")
+   console.log(imageHoverOn.innerHTML);
+   console.log('clicked')
+  });
+});
 
-projectOverlay.addEventListener("touchend", function() {
-  projectOverlay.classList.remove("project__overlay--mobile-hover")
-})
+document.querySelectorAll('.project__overlay').forEach(function(imageHoverOff) {
+  imageHoverOff.addEventListener('touchend', function() {
+   imageHoverOff.classList.remove("project__overlay--mobile-hover")
+   console.log('clicked off')
+  });
+});
 
-
-const projectOverlayTwo = document.getElementsByClassName("project__overlay")[1]
-
-projectOverlayTwo.addEventListener("touchstart", function() {
-  projectOverlayTwo.classList.add("project__overlay--mobile-hover")
-})
-
-projectOverlayTwo.addEventListener("touchend", function() {
-  projectOverlayTwo.classList.remove("project__overlay--mobile-hover")
-})
-
-
-const projectOverlayThree = document.getElementsByClassName("project__overlay")[2]
-
-projectOverlayThree.addEventListener("touchstart", function() {
-  projectOverlayThree.classList.add("project__overlay--mobile-hover")
-})
-
-projectOverlayThree.addEventListener("touchend", function() {
-  projectOverlayThree.classList.remove("project__overlay--mobile-hover")
-})
-
-
-const projectOverlayFour = document.getElementsByClassName("project__overlay")[3]
-
-projectOverlayFour.addEventListener("touchstart", function() {
-  projectOverlayFour.classList.add("project__overlay--mobile-hover")
-})
-
-projectOverlayFour.addEventListener("touchend", function() {
-  projectOverlayFour.classList.remove("project__overlay--mobile-hover")
-})
